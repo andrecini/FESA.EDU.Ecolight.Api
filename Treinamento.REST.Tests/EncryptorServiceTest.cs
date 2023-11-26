e3c147c76dfc9966106d7b90946124260ace3356
andrepresys <andre.presys@gmail.com>
2023-11-26 17:28:56 -0300

    Testes unitários da EncryptorService

---
+++ Treinamento.REST.Tests/EncryptorServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treinamento.REST.Services.Services;
+
+namespace Treinamento.REST.Tests
+{
+    public class EncryptorServiceTest
+    {
+        [Theory]
+        [InlineData("teste123")]
+        [InlineData("senha123")]
+        [InlineData("12345678")]
+        public void EncryptAndDecrypt(string password)
+        {
+            var service = new EncryptorService();
+
+            var encrypted = service.Encrypt(password);
+            var decrypt = service.Decrypt(encrypted);
+
+            Assert.NotNull(encrypted);
+            Assert.NotNull(decrypt);
+            Assert.Equal(password, decrypt);
+        }
+    }
+}
